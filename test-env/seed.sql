ba2828ffd778f3104f6872037ab658a2fd3b5503
Michael Reinegger <reinemic2.0@gmail.com>
2026-01-22 20:29:24 +0100

    feat: use INSERT OR IGNORE in seed data for devices and schedules

--- test-env/seed.sql
+++ test-env/seed.sql
@@ -1,2 +1,2 @@
-INSERT INTO devices (name, hostname, ip, ssh_user, ssh_port) VALUES ('Node 1', 'node1', 'node1', 'root', 22), ('Node 2', 'node2', 'node2', 'root', 22);
-INSERT INTO schedules (type, cron, enabled) VALUES ('ping', '*/1 * * * *', 1), ('speed', '*/5 * * * *', 1);
+INSERT OR IGNORE INTO devices (name, hostname, ip, ssh_user, ssh_port) VALUES ('Node 1', 'node1', 'node1', 'root', 22), ('Node 2', 'node2', 'node2', 'root', 22);
+INSERT OR IGNORE INTO schedules (type, cron, enabled) VALUES ('ping', '*/1 * * * *', 1), ('speed', '*/5 * * * *', 1);
